ac9c70e938cd1b53235744b42cdc55c3ab7dcac9
Henning Brinkmann <github@henning-brinkmann.de>
2017-09-10 21:41:46 +0200

    util.py: Use dateutil for TZs. getNow, getStartOfDay

--- animations/util.py
+++ animations/util.py
@@ -1,10 +1,22 @@
 import ephem as ep
 import bibliopixel.colors as colors
 import config
+import datetime
+from dateutil.tz import *
+
+def getNow():
+    return datetime.datetime.now(tzlocal())
+
+def getStartOfDay(date):
+    return datetime.datetime(date.year, date.month, date.day, tzinfo = date.tzinfo)
 
 def getSunColor(date):
-    current = ep.Date((date.year, date.month, date.day, date.hour, date.minute, date.second))
-    startOfDay = ep.Date((date.year, date.month, date.day))
+    utc_dt = date.astimezone(tzutc())
+    startOfDay_dt = getStartOfDay(date)
+    startOfDay_utc_dt = startOfDay_dt.astimezone(tzutc())
+
+    current = ep.Date((utc_dt.year, utc_dt.month, utc_dt.day, utc_dt.hour, utc_dt.minute, utc_dt.second))
+    startOfDay = ep.Date((startOfDay_utc_dt.year, startOfDay_utc_dt.month, startOfDay_utc_dt.day, startOfDay_utc_dt.hour, startOfDay_utc_dt.minute, startOfDay_utc_dt.second))
     endOfDay = startOfDay + 1
 
     observer = ep.Observer()
