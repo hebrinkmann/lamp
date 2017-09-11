70b58cac62e3e3d259df1ca1f349b18e971a1d72
Henning Brinkmann <github@henning-brinkmann.de>
2017-09-11 21:46:41 +0200

    util.py: Cache for suncolor.

--- animations/util.py
+++ animations/util.py
@@ -10,21 +10,31 @@ def getNow():
 def getStartOfDay(date):
     return datetime.datetime(date.year, date.month, date.day, tzinfo = date.tzinfo)
 
+sunColorCache = {}
+
 def getSunColor(date):
     utc_dt = date.astimezone(tzutc())
     startOfDay_dt = getStartOfDay(date)
     startOfDay_utc_dt = startOfDay_dt.astimezone(tzutc())
 
     current = ep.Date((utc_dt.year, utc_dt.month, utc_dt.day, utc_dt.hour, utc_dt.minute, utc_dt.second))
-    startOfDay = ep.Date((startOfDay_utc_dt.year, startOfDay_utc_dt.month, startOfDay_utc_dt.day, startOfDay_utc_dt.hour, startOfDay_utc_dt.minute, startOfDay_utc_dt.second))
+
+    startOfDay = ep.Date((
+        startOfDay_utc_dt.year, startOfDay_utc_dt.month, startOfDay_utc_dt.day, startOfDay_utc_dt.hour,
+        startOfDay_utc_dt.minute, startOfDay_utc_dt.second))
     endOfDay = startOfDay + 1
 
-    observer = ep.Observer()
-    observer.lat = '54.0'
-    observer.lon = '10.0'
-    observer.date = startOfDay
-    sunrise = observer.next_rising(ep.Sun())
-    sunset = observer.next_setting(ep.Sun())
+    if sunColorCache.get('date') != startOfDay_dt:
+        observer = ep.Observer()
+        observer.lat = '54.0'
+        observer.lon = '10.0'
+        observer.date = startOfDay
+        sunColorCache['date'] = startOfDay_dt
+        sunColorCache['sunrise'] = observer.next_rising(ep.Sun())
+        sunColorCache['sunset'] = observer.next_setting(ep.Sun())
+
+    sunrise = sunColorCache['sunrise']
+    sunset = sunColorCache['sunset']
 
     result = colors.Black
 
